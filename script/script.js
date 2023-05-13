12eb435e62e3f79a91444f72024bd19217dc29bd
EsraaMohammedAhmed <97108623+EsraaMohammedAhmed@users.noreply.github.com>
2023-05-14 00:58:49 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,92 @@
+// Open the full screen
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+/************************************************/
+
+/********************like icon**************************/
+var x = document.querySelectorAll(".btn");
+x.forEach((e) => {
+  e.addEventListener("click", () => {
+    e.classList.toggle("active");
+  });
+});
+
+/***************************************** *
+/**********************product**********************************/
+
+let preveiwContainer = document.querySelector(".products-preview");
+let previewBox = preveiwContainer.querySelectorAll(".preview");
+
+document.querySelectorAll(".products-container .product").forEach((product) => {
+  product.onclick = () => {
+    preveiwContainer.style.display = "flex";
+    let name = product.getAttribute("data-name");
+    previewBox.forEach((preview) => {
+      let target = preview.getAttribute("data-target");
+      if (name == target) {
+        preview.classList.add("active");
+      }
+    });
+  };
+});
+
+previewBox.forEach((close) => {
+  close.querySelector(".fa-times").onclick = () => {
+    close.classList.remove("active");
+    preveiwContainer.style.display = "none";
+  };
+});
+/************************buttons size***********************************/
+const currentProductSizes = document.querySelectorAll(".size");
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+/*************************add to card button********************************/
+
+const payment = document.querySelector(".payment");
+const closs = document.querySelector(".closs");
+
+payment.addEventListener("click", () => {
+  closs.style.display = "flex";
+});
+
+/****************************************************/
+
+/*************************************login****************************************** */
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
